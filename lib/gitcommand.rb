55f0aaece90da0b7d8e621d90a14aa890f184d28
Your Name <you@example.com>
2015-08-01 08:06:39 +0000

    gitcommand fix

--- lib/gitcommand.rb
+++ lib/gitcommand.rb
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-#require "gitcommand/version"
+#require "Gitcommand/version"
 
 module Gitcommand
-	def self.gittest
+	def gittest
 		puts("リポジトリを作成するコマンドを入力してください")
-		str01 = gets.to_i
-		if str01 == "git init"
+		str01 = gets.to_s
+		str01 = str01.strip
+
+		if str01=='git init' then
 			puts ("等しい")
 		else
 			puts ("間違いです。正解は「git init」です。")
 		end
 	end
+        module_function :gittest
 end
+Gitcommand.gittest
