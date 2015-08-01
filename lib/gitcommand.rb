8aa6cc2bdcb785456203a94e59ab78ba428e5089
vagrant <weblab443@gmail.com>
2015-08-01 09:06:05 +0000

    delfileoldfile

--- lib/gitcommand.rb
+++ lib/gitcommand.rb
@@ -3,15 +3,47 @@ require "gitcommand/version"
 
 module Gitcommand
 	def gittest
-		puts("リポジトリを作成するコマンドを入力してください")
-		str01 = gets.to_s
-		str01 = str01.strip
+               puts("リポジトリを作成するコマンドを入力してください")
+                str01 = gets.to_s
+                str01 = str01.strip
 
-		if str01=='git init' then
-			puts ("等しい")
-		else
-			puts ("間違いです。正解は「git init」です。")
-		end
+                if str01=='git init' then
+                        puts ("正解です")
+                else
+                        puts ("間違いです。正解は「git init」です。")
+                end
+
+               puts("ブランチの切り替えをするコマンドを入力してください")
+                str02 = gets.to_s
+                str02 = str02.strip
+
+                if str02=='git checkout' then
+                        puts ("正解です")
+                else
+                        puts ("間違いです。正解は「git checkout」です。")
+                end
+
+               puts("ブランチの情報、一覧を表示させるコマンドを入力してください")
+                str03 = gets.to_s
+                str03 = str03.strip
+
+                if str03=='git branch' then
+                        puts ("正解です")
+                else
+                        puts ("間違いです。正解は「git branch」です。")
+                end
+
+               puts("ほかのリポジトリの変更点をローカルリポジトリにマージする")
+                str04 = gets.to_s
+                str04 = str04.strip
+
+                if str04=='git pull' then
+                        puts ("正解です")
+                else
+                        puts ("間違いです。正解は「git pull」です。")
+                end
+
+
 	end
         module_function :gittest
 end
